141d75782a02144bed5aac79ec64edc39b896672
yasklopp <j2371925@gmail.com>
2021-12-12 01:00:18 +0800

    coupon

---
+++ SA_12/WEB-INF/classes/ncu/im3069/demo/app/CouponHelper.java
@@ -0,0 +1,376 @@
+package ncu.im3069.demo.app;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.TimeZone;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import ncu.im3069.demo.util.DBMgr;
+
+public class CouponHelper {
+private CouponHelper() {
+        
+    }
+    
+    private static CouponHelper ch;
+    private Connection conn = null;
+    private PreparedStatement pres = null;
+    
+    public static CouponHelper getHelper() {
+        /** Singleton檢查是否已經有ProductHelper物件，若無則new一個，若有則直接回傳 */
+        if(ch == null) ch = new CouponHelper();
+        
+        return ch;
+    }
+    public JSONObject getAll() {
+        /** 新建一個 Member 物件之 m 變數，用於紀錄每一位查詢回之會員資料 */
+        Coupon c = null;
+        /** 用於儲存所有檢索回之會員，以JSONArray方式儲存 */
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
+        
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT * FROM `missa`.`coupon`";
+            
+            /** 將參數回填至SQL指令當中，若無則不用只需要執行 prepareStatement */
+            pres = conn.prepareStatement(sql);
+            /** 執行查詢之SQL指令並記錄其回傳之資料 */
+            rs = pres.executeQuery();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+            
+            /** 透過 while 迴圈移動pointer，取得每一筆回傳資料 */
+            while(rs.next()) {
+                /** 每執行一次迴圈表示有一筆資料 */
+                row += 1;
+                
+                /** 將 ResultSet 之資料取出 */
+                int coupon_id = rs.getInt("id");
+                String name = rs.getString("name");
+                double discount = rs.getDouble("discount");
+                String coupon_start_time = String.valueOf(rs.getTimestamp("start_time"));
+                String end_times = String.valueOf(rs.getTimestamp("end_time"));
+              
+                
+                /** 將每一筆會員資料產生一名新Member物件 */
+                c = new Coupon(coupon_id, name, discount, coupon_start_time, end_times);
+                /** 取出該名會員之資料並封裝至 JSONsonArray 內 */
+                jsa.put(c.getData());
+            }
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+        
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+        
+        /** 將SQL指令、花費時間、影響行數與所有會員資料之JSONArray，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+        response.put("data", jsa);
+
+        return response;
+    }
+    
+    public JSONObject getByID(int id) {
+        /** 新建一個 Member 物件之 m 變數，用於紀錄每一位查詢回之會員資料 */
+        Coupon c = null;
+        /** 用於儲存所有檢索回之會員，以JSONArray方式儲存 */
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
+        
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT * FROM `missa`.`coupon` WHERE `id` = ? LIMIT 1";
+            
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setInt(1, id);
+            /** 執行查詢之SQL指令並記錄其回傳之資料 */
+            rs = pres.executeQuery();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+            
+            /** 透過 while 迴圈移動pointer，取得每一筆回傳資料 */
+            /** 正確來說資料庫只會有一筆該會員編號之資料，因此其實可以不用使用 while 迴圈 */
+            while(rs.next()) {
+                /** 每執行一次迴圈表示有一筆資料 */
+                row += 1;
+                
+                /** 將 ResultSet 之資料取出 */
+                int coupon_id = rs.getInt("id");
+                String name = rs.getString("name");
+                double discount = rs.getDouble("discount");
+                String coupon_start_time = String.valueOf(rs.getTimestamp("start_time"));
+                String end_times = String.valueOf(rs.getTimestamp("end_times"));
+                
+                /** 將每一筆會員資料產生一名新Member物件 */
+                c = new Coupon(coupon_id, name, discount, coupon_start_time, end_times);
+                /** 取出該名會員之資料並封裝至 JSONsonArray 內 */
+                jsa.put(c.getData());
+            }
+            
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+        
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+        
+        /** 將SQL指令、花費時間、影響行數與所有會員資料之JSONArray，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+        response.put("data", jsa);
+
+        return response;
+    }
+    
+    public JSONObject create(Coupon c) {
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "INSERT INTO `missa`.`coupon`(`name`, `discount`, `start_time`, `end_time`)"
+                    + " VALUES(?, ?, ?, ?)";
+            
+            /** 取得所需之參數 */
+            String name = c.getName();
+            double discount = c.getDiscount();
+            String coupon_start_time = c.getStart_time();
+            String end_time = c.getEnd_time();
+            
+            SimpleDateFormat sdf = null;
+            sdf = new SimpleDateFormat("yyyy-MM-dd");
+            sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+            Date date = null;
+            date = sdf.parse(coupon_start_time);
+            java.sql.Timestamp s_coupon_start_time = new java.sql.Timestamp(date.getTime());
+            date = sdf.parse(end_time);
+            java.sql.Timestamp s_end_time = new java.sql.Timestamp(date.getTime());
+            /*Date udHiredate = null;
+            udHiredate = simpleDateFormat.parse(coupon_start_time);
+            java.sql.Date sdcoupon_start_time = new java.sql.Date(udHiredate.getTime());           
+            udHiredate = simpleDateFormat.parse(end_time);
+            System.out.print(udHiredate);
+            java.sql.Date sdend_time = new java.sql.Date(udHiredate.getTime());
+            System.out.print(sdend_time);*/
+            
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setString(1, name);
+            pres.setDouble(2, discount);
+            pres.setTimestamp(3, s_coupon_start_time);
+            pres.setTimestamp(4, s_end_time);
+            //pres.setTimestamp(3, Timestamp.valueOf(coupon_start_time));
+            //pres.setTimestamp(4, Timestamp.valueOf(end_time));
+            //System.out.print(Timestamp.valueOf(coupon_start_time));
+            
+            /** 執行新增之SQL指令並記錄影響之行數 */
+            row = pres.executeUpdate();
+            
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(pres, conn);
+        }
+
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+
+        /** 將SQL指令、花費時間與影響行數，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("time", duration);
+        response.put("row", row);
+
+        return response;
+    }
+    
+    public JSONObject update(Coupon c) {
+        /** 紀錄回傳之資料 */
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "Update `missa`.`coupon` SET `name` = ?, `discount` = ? , `start_time` = ? , `end_time` = ? WHERE `id` = ?";
+            /** 取得所需之參數 */
+            int id = c.getID();
+            String name = c.getName();
+            double discount = c.getDiscount();
+            String coupon_start_time = c.getStart_time();
+            String end_time = c.getEnd_time();
+            
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql); 
+            pres.setString(1, name);
+            pres.setDouble(2, discount);
+            pres.setTimestamp(3, Timestamp.valueOf(coupon_start_time));
+            pres.setTimestamp(4, Timestamp.valueOf(end_time));
+            pres.setInt(5, id);
+            /** 執行更新之SQL指令並記錄影響之行數 */
+            row = pres.executeUpdate();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(pres, conn);
+        }
+        
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+        
+        /** 將SQL指令、花費時間與影響行數，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+        response.put("data", jsa);
+
+        return response;
+    }
+    
+    public JSONObject deleteByID(int id) {
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
+        
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            
+            /** SQL指令 */
+            String sql = "DELETE FROM `missa`.`coupon` WHERE `id` = ? LIMIT 1";
+            
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setInt(1, id);
+            /** 執行刪除之SQL指令並記錄影響之行數 */
+            row = pres.executeUpdate();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+            
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+        
+        /** 將SQL指令、花費時間與影響行數，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+
+        return response;
+    }
+}
---
+++ SA_12/WEB-INF/classes/ncu/im3069/demo/controller/CouponController.java
@@ -0,0 +1,155 @@
+package ncu.im3069.demo.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import ncu.im3069.demo.app.Coupon;
+import ncu.im3069.demo.app.CouponHelper;
+import ncu.im3069.demo.app.Member;
+import ncu.im3069.demo.app.MemberHelper;
+import ncu.im3069.demo.app.Product;
+import ncu.im3069.tools.JsonReader;
+
+/**
+ * Servlet implementation class CouponController
+ */
+@WebServlet("/api/CouponController.do")
+public class CouponController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private CouponHelper ch =  CouponHelper.getHelper();
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CouponController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
+        JsonReader jsr = new JsonReader(request);
+        /** 若直接透過前端AJAX之data以key=value之字串方式進行傳遞參數，可以直接由此方法取回資料 */
+        String id = jsr.getParameter("id");
+
+        JSONObject resp = new JSONObject();
+        /** 判斷該字串是否存在，若存在代表要取回購物車內產品之資料，否則代表要取回全部資料庫內產品之資料 */
+        if (!id.isEmpty()) {
+          JSONObject query = ch.getByID(Integer.parseInt(id));
+          resp.put("status", "200");
+          resp.put("message", "所有購物車之商品資料取得成功");
+          resp.put("response", query);
+        }
+        else {
+          JSONObject query = ch.getAll();
+
+          resp.put("status", "200");
+          resp.put("message", "所有商品資料取得成功");
+          resp.put("response", query);
+        }
+
+        jsr.response(resp, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
+        JsonReader jsr = new JsonReader(request);
+        JSONObject jso = jsr.getObject();
+        
+        /** 取出經解析到JSONObject之Request參數 */
+        String name = jso.getString("name");
+        String discount = jso.getString("discount");
+        String start_time = jso.getString("start_time");
+        String end_time = jso.getString("end_time");
+        
+        /** 建立一個新的會員物件 */
+        Coupon c = new Coupon(name, Double.parseDouble(discount), start_time, end_time);
+        
+        /** 後端檢查是否有欄位為空值，若有則回傳錯誤訊息 */
+        if(name.isEmpty() || discount.isEmpty() || start_time.isEmpty() || end_time.isEmpty()) {
+            /** 以字串組出JSON格式之資料 */
+            String resp = "{\"status\": \'400\', \"message\": \'欄位不能有空值\', \'response\': \'\'}";
+            /** 透過JsonReader物件回傳到前端（以字串方式） */
+            jsr.response(resp, response);
+        }
+        /** 透過MemberHelper物件的checkDuplicate()檢查該會員電子郵件信箱是否有重複 */
+        else  {
+            /** 透過MemberHelper物件的create()方法新建一個會員至資料庫 */
+            JSONObject data = ch.create(c);
+            
+            /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+            JSONObject resp = new JSONObject();
+            resp.put("status", "200");
+            resp.put("message", "成功! 註冊會員資料...");
+            resp.put("response", data);
+            
+            /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+            jsr.response(resp, response);
+        }
+ 
+	}
+
+	
+	public void doDelete(HttpServletRequest request, HttpServletResponse response)
+	        throws ServletException, IOException {
+	        /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
+	        JsonReader jsr = new JsonReader(request);
+	        JSONObject jso = jsr.getObject();
+	        
+	        /** 取出經解析到JSONObject之Request參數 */
+	        int id = jso.getInt("id");
+	        
+	        /** 透過MemberHelper物件的deleteByID()方法至資料庫刪除該名會員，回傳之資料為JSONObject物件 */
+	        JSONObject query = ch.deleteByID(id);
+	        
+	        /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+	        JSONObject resp = new JSONObject();
+	        resp.put("status", "200");
+	        resp.put("message", "會員移除成功！");
+	        resp.put("response", query);
+
+	        /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+	        jsr.response(resp, response);
+	    }
+	
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)
+		    throws ServletException, IOException {
+		/** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
+        JsonReader jsr = new JsonReader(request);
+        JSONObject jso = jsr.getObject();
+        
+        /** 取出經解析到JSONObject之Request參數 */
+        int id = jso.getInt("id");
+        String name = jso.getString("name");
+        String discount = jso.getString("discount");
+        String start_time = jso.getString("start_time");
+        String end_time = jso.getString("end_time");
+        
+
+        /** 透過傳入之參數，新建一個以這些參數之會員Member物件 */
+        Coupon c = new Coupon(id, name, Double.parseDouble(discount), start_time, end_time);
+        
+        /** 透過Member物件的update()方法至資料庫更新該名會員資料，回傳之資料為JSONObject物件 */
+        JSONObject data = c.update();
+        
+        /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+        JSONObject resp = new JSONObject();
+        resp.put("status", "200");
+        resp.put("message", "成功! 更新會員資料...");
+        resp.put("response", data);
+        
+        /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+        jsr.response(resp, response); 
+		}
+}

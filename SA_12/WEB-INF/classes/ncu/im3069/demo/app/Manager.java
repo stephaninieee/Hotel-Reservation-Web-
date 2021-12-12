12a63a3725ce8742e20cd180a9ba0e9d81722782
uCedric <smpss96271@gmail.com>
2021-12-12 11:19:03 +0800

    finished the add,delete,update of manager

--- SA_12/WEB-INF/classes/ncu/im3069/demo/app/Manager.java
+++ SA_12/WEB-INF/classes/ncu/im3069/demo/app/Manager.java
@@ -1,5 +1,7 @@
 package ncu.im3069.demo.app;
 
+import java.util.Calendar;
+
 import org.json.*;
 
 public class Manager {
@@ -10,7 +12,6 @@ public class Manager {
 	private String password;
 	private String phone;
 	private int login_times;
-	private String status;
 	private ManagerHelper mnh =  ManagerHelper.getHelper();
 	
 	public Manager(String name,String email,String password,String phone){
@@ -28,17 +29,16 @@ public class Manager {
         this.name = name;
         /** 取回原有資料庫內該名會員之更新時間分鐘數與組別 */
         getLoginTimesStatus();
-        /** 計算會員之組別 */
-        calcAccName();
+        
     }
 	
-	public Manager(int id, String email, String password, String name, int login_times, String status) {
+	public Manager(int id, String email, String password, String name,String phone, int login_times) {
         this.id = id;
         this.email = email;
         this.password = password;
         this.name = name;
-        this.login_times = login_times;
-        this.status = status;
+        this.phone = phone;
+        this.login_times = login_times;   
     }
 	
 	public int getId() {
@@ -59,10 +59,7 @@ public class Manager {
 	public int getLogin_times() {
 		return this.login_times;
 	}
-	public String getStatus() {
-
-		return this.status;
-	}
+	
 
 	public JSONObject getData() {
         /** 透過JSONObject將該名會員所需之資料全部進行封裝*/ 
@@ -72,10 +69,33 @@ public class Manager {
         jso.put("email", getEmail());
         jso.put("password", getPassword());
         jso.put("login_times", getLogin_times());
-        jso.put("status", getStatus());
+        
         
         return jso;
     }
 
-	
+	private void getLoginTimesStatus() {
+        /** 透過MemberHelper物件，取得儲存於資料庫的更新時間分鐘數與會員組別 */
+        JSONObject data = mnh.getLoginTimesStatus(this);
+        /** 將資料庫所儲存該名會員之相關資料指派至Member物件之屬性 */
+        this.login_times = data.getInt("login_times");
+        
+    }
+
+	public JSONObject update() {
+        /** 新建一個JSONObject用以儲存更新後之資料 */
+        JSONObject data = new JSONObject();
+        /** 取得更新資料時間（即現在之時間）之分鐘數 */
+        Calendar calendar = Calendar.getInstance();
+        this.login_times = calendar.get(Calendar.MINUTE);
+        
+        if(this.id != 0) {
+            /** 若有則將目前更新後之資料更新至資料庫中 */
+            mnh.updateLoginTimes(this);
+            /** 透過MemberHelper物件，更新目前之會員資料置資料庫中 */
+            data = mnh.update(this);
+        }
+        
+        return data;
+    }
 }
--- SA_12/WEB-INF/classes/ncu/im3069/demo/controller/MemberController.java
+++ SA_12/WEB-INF/classes/ncu/im3069/demo/controller/MemberController.java
@@ -37,48 +37,47 @@ public class MemberController extends HttpServlet {
      * @throws IOException Signals that an I/O exception has occurred.
      */
     public void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-        /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
-        JsonReader jsr = new JsonReader(request);
-        JSONObject jso = jsr.getObject();
-        
-        /** 取出經解析到JSONObject之Request參數 */
-        String email = jso.getString("email");
-        String password = jso.getString("password");
-        String name = jso.getString("name");
-        
-        /** 建立一個新的會員物件 */
-        Member m = new Member(email, password, name);
-        
-        /** 後端檢查是否有欄位為空值，若有則回傳錯誤訊息 */
-        if(email.isEmpty() || password.isEmpty() || name.isEmpty()) {
-            /** 以字串組出JSON格式之資料 */
-            String resp = "{\"status\": \'400\', \"message\": \'欄位不能有空值\', \'response\': \'\'}";
-            /** 透過JsonReader物件回傳到前端（以字串方式） */
-            jsr.response(resp, response);
-        }
-        /** 透過MemberHelper物件的checkDuplicate()檢查該會員電子郵件信箱是否有重複 */
-        else if (!mh.checkDuplicate(m)) {
-            /** 透過MemberHelper物件的create()方法新建一個會員至資料庫 */
-            JSONObject data = mh.create(m);
+            throws ServletException, IOException {
+            /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
+            JsonReader jsr = new JsonReader(request);
+            JSONObject jso = jsr.getObject();
             
-            /** 新建一個JSONObject用於將回傳之資料進行封裝 */
-            JSONObject resp = new JSONObject();
-            resp.put("status", "200");
-            resp.put("message", "成功! 註冊會員資料...");
-            resp.put("response", data);
+            /** 取出經解析到JSONObject之Request參數 */
+            String email = jso.getString("email");
+            String password = jso.getString("password");
+            String name = jso.getString("name");
             
-            /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
-            jsr.response(resp, response);
+            /** 建立一個新的會員物件 */
+            Member m = new Member(email, password, name);
+            
+            /** 後端檢查是否有欄位為空值，若有則回傳錯誤訊息 */
+            if(email.isEmpty() || password.isEmpty() || name.isEmpty()) {
+                /** 以字串組出JSON格式之資料 */
+                String resp = "{\"status\": \'400\', \"message\": \'欄位不能有空值\', \'response\': \'\'}";
+                /** 透過JsonReader物件回傳到前端（以字串方式） */
+                jsr.response(resp, response);
+            }
+            /** 透過MemberHelper物件的checkDuplicate()檢查該會員電子郵件信箱是否有重複 */
+            else if (!mh.checkDuplicate(m)) {
+                /** 透過MemberHelper物件的create()方法新建一個會員至資料庫 */
+                JSONObject data = mh.create(m);
+                
+                /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+                JSONObject resp = new JSONObject();
+                resp.put("status", "200");
+                resp.put("message", "成功! 註冊會員資料...");
+                resp.put("response", data);
+                
+                /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+                jsr.response(resp, response);
+            }
+            else {
+                /** 以字串組出JSON格式之資料 */
+                String resp = "{\"status\": \'400\', \"message\": \'新增帳號失敗，此E-Mail帳號重複！\', \'response\': \'\'}";
+                /** 透過JsonReader物件回傳到前端（以字串方式） */
+                jsr.response(resp, response);
+            }
         }
-        else {
-            /** 以字串組出JSON格式之資料 */
-            String resp = "{\"status\": \'400\', \"message\": \'新增帳號失敗，此E-Mail帳號重複！\', \'response\': \'\'}";
-            /** 透過JsonReader物件回傳到前端（以字串方式） */
-            jsr.response(resp, response);
-        }
-    }
-
     /**
      * 處理Http Method請求GET方法（取得資料）
      *

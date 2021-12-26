3c38778613aa2ea56fdaaf062b75e2028a38e63d
stephaninieee <steph.20010103@gmail.com>
2021-12-27 01:40:37 +0800

    stephaninieee MemberLogin.html errors

--- SA_12/WEB-INF/classes/ncu/im3069/demo/controller/MemberController.java
+++ SA_12/WEB-INF/classes/ncu/im3069/demo/controller/MemberController.java
@@ -95,6 +95,7 @@ public class MemberController extends HttpServlet {
         JsonReader jsr = new JsonReader(request);
         /** 若直接透過前端AJAX之data以key=value之字串方式進行傳遞參數，可以直接由此方法取回資料 */
         String id = jsr.getParameter("id");
+        String email= jsr.getParameter("email");
         
         /** 判斷該字串是否存在，若存在代表要取回個別會員之資料，否則代表要取回全部資料庫內會員之資料 */
         if (id.isEmpty()) {
@@ -114,6 +115,7 @@ public class MemberController extends HttpServlet {
             /** 透過MemberHelper物件的getByID()方法自資料庫取回該名會員之資料，回傳之資料為JSONObject物件 */
             JSONObject query = mh.getByID(id);
             
+            
             /** 新建一個JSONObject用於將回傳之資料進行封裝 */
             JSONObject resp = new JSONObject();
             resp.put("status", "200");
@@ -122,8 +124,34 @@ public class MemberController extends HttpServlet {
     
             /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
             jsr.response(resp, response);
-        }
+             
+        }    
+        if (email.isEmpty()) {
+        	/** 透過MemberHelper物件之getAll()方法取回所有會員之資料，回傳之資料為JSONObject物件 */
+        	JSONObject query = mh.getAll();         
+        	/** 新建一個JSONObject用於將回傳之資料進行封裝 */ 
+        	JSONObject resp = new JSONObject();
+        	resp.put("status", "200");
+        	resp.put("message", "所有房東資料取得成功");
+        	resp.put("response", query);
+
+        	/** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+        	jsr.response(resp, response);
+    	}
+    	else {                     	
+    		JSONObject data = mh.getByEmail(email);
+        	/** 新建一個JSONObject用於將回傳之資料進行封裝 */
+        	JSONObject resp = new JSONObject();            	            	            		
+        	resp.put("status", "200");
+        	resp.put("message", "房東資料取得成功");
+        	resp.put("response", data);  
+        	/** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+        	jsr.response(resp, response);
+    	
+    	}
+        	
     }
+       
 
     /**
      * 處理Http Method請求DELETE方法（刪除）
